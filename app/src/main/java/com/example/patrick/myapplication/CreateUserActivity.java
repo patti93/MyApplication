8903a27c03c21527dab2ed28dbcd2aad6781918f
Patrick <patrick.muhl93@gmail.com>
2019-02-11 18:13:15 +0100

    Login validiert nun alle Felder außer den inhalt der Namen

--- app/src/main/java/com/example/patrick/myapplication/CreateUserActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/CreateUserActivity.java
@@ -38,17 +38,17 @@ public class CreateUserActivity extends AppCompatActivity {
         if(isEmpty(inputBday) || isEmpty(inputFirstName) || isEmpty(inputMail) || isEmpty(inputName) || isEmpty(inputPassword) || isEmpty(inputPassword2)){
             return 4;
         }
-        if(!password.equals(password2)) return 1;
+        Pattern email_pattern = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
+        if(!email_pattern.matcher(mail).matches()) return 2;
 
         if(password.length() < 8) return 3;
 
-        Pattern email_pattern = Pattern.compile("[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]+");
+        if(!password.equals(password2)) return 1;
 
-        if(!email_pattern.matcher("mail").matches()) return 2;
+        Pattern bday_pattern = Pattern.compile("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+        if (!bday_pattern.matcher(bday).matches()) return 5;
 
         return 0;
-
-
 
 
     }
