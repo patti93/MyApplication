9d4fcec4d3ec20b4141a4288964c12539f0adc99
Schremm <florian.fecht@gmail.com>
2019-02-13 16:43:28 +0100

    Suchfeld in WG Suche hinzugefügt.
    Hauptmenü mit WG Gründung verknüpft. Formular wird noch nicht validiert.

--- app/src/main/java/com/example/patrick/myapplication/FoundWGActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/FoundWGActivity.java
@@ -1,7 +1,9 @@
 package com.example.patrick.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FoundWGActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FoundWGActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found_wg);
+    }
+
+    public void redirect(View view){
+        Intent intent = new Intent(this,MainMenuActivity.class);
+        startActivity(intent);
     }
 }
