9c984a0f2a3feb219e7383248327268779fca31c
Schremm <florian.fecht@gmail.com>
2019-02-26 13:16:37 +0100

    WG Detail Seite hinzugefügt
    Buttons erstmal ohne Funktion.

--- app/src/main/java/com/example/patrick/myapplication/ShowWGActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/ShowWGActivity.java
@@ -1,7 +1,9 @@
 package com.example.patrick.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowWGActivity extends AppCompatActivity {
 
@@ -9,5 +11,41 @@ public class ShowWGActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_wg);
+
+        String wgName;
+        String address;
+        String description;
+        String password;
+        ActiveResident activeResident;
+        WG4U_DataSource dataSource;
+
+        dataSource = new WG4U_DataSource(this);
+        dataSource.open();
+        activeResident = new ActiveResident(this);
+
+
+
+        TextView textView_wg_name = findViewById(R.id.print_wg_name);
+        wgName = dataSource.findResidentsWg(activeResident.getActiveResident()).getName();
+        textView_wg_name.setText(wgName);
+
+
+        TextView textView_address = findViewById(R.id.print_address);
+        address =   dataSource.findResidentsWg(activeResident.getActiveResident()).getStreet() +
+                    dataSource.findResidentsWg(activeResident.getActiveResident()).getHnr() +
+                    "\n" +
+                    dataSource.findResidentsWg(activeResident.getActiveResident()).getZipCode() +
+                    dataSource.findResidentsWg(activeResident.getActiveResident()).getTown() +
+                    "\n" +
+                    dataSource.findResidentsWg(activeResident.getActiveResident()).getCountry();
+        textView_address.setText(address);
+
+        TextView textView_description = findViewById(R.id.print_description);
+        description = dataSource.findResidentsWg(activeResident.getActiveResident()).getDescription();
+        textView_description.setText(description);
+
+        TextView textView_wg_password = findViewById(R.id.print_password);
+        password = dataSource.findResidentsWg(activeResident.getActiveResident()).getPassword();
+        textView_wg_password.setText(password);
     }
 }
