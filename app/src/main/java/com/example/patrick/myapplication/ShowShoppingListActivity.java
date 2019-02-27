ee62189ade7b73d3ab689d719d2787a3e5318d8b
Schremm <florian.fecht@gmail.com>
2019-02-27 01:43:37 +0100

    Layout Einkaufsliste fertig

--- app/src/main/java/com/example/patrick/myapplication/ShowShoppingListActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/ShowShoppingListActivity.java
@@ -1,13 +1,21 @@
 package com.example.patrick.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class ShowShoppingListActivity extends AppCompatActivity {
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_shopping_list);
+
     }
 }
