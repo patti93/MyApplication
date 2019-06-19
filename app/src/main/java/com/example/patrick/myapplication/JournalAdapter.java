3e00feac4ae8ad80ed35b45e58da70d30febe6a4
Patrick <patrick.muhl93@gmail.com>
2019-06-19 23:33:41 +0200

    Beta Release

--- app/src/main/java/com/example/patrick/myapplication/JournalAdapter.java
+++ app/src/main/java/com/example/patrick/myapplication/JournalAdapter.java
@@ -1,9 +1,12 @@
 package com.example.patrick.myapplication;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,17 +21,32 @@ public class JournalAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        JournalEntry entry = (JournalEntry) getItem(position);
 
 
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.journal_item, parent, false);
+        }
 
 
+        TextView sent_by = convertView.findViewById(R.id.journalListViewItemSentBy);
+        TextView message = convertView.findViewById(R.id.journalListViewItemMessage);
+        TextView date = convertView.findViewById(R.id.journalListViewItemDate);
+        TextView time = convertView.findViewById(R.id.journalListViewItemTime);
 
 
+        String hour = Integer.toString(entry.getHour());
+        if(hour.length()==1) hour = "0" + hour;
+        String minute = Integer.toString(entry.getMinute());
+        if(minute.length()==1) minute = "0" + minute;
+        String t =  hour + ":" + minute;
 
+        sent_by.setText(entry.getSent_by());
+        message.setText(entry.getMessage());
+        date.setText(entry.getDate());
+        time.setText(t);
+        Log.d("INFO",t);
 
-
-
-
-        return super.getView(position, convertView, parent);
+        return convertView;
     }
 }
--- app/src/main/java/com/example/patrick/myapplication/ShowJournalActivity.java
+++ app/src/main/java/com/example/patrick/myapplication/ShowJournalActivity.java
@@ -2,12 +2,153 @@ package com.example.patrick.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowJournalActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = ShowJournalActivity.class.getSimpleName();
+    
+    public JournalAdapter journalAdapter;
+    public ListView journalListView;
+    public ArrayList<JournalEntry> journalEntries;
+    
+    public ActiveWG activeWG;
+    
+    public ActiveResident activeResident;
+
+    public EditText messageInput;
+
+    SimpleDateFormat sdf;
+    String currentDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        activeWG = new ActiveWG(getApplicationContext());
+        activeResident = new ActiveResident(getApplicationContext());
+        
         setContentView(R.layout.activity_show_journal);
+        journalListView = findViewById(R.id.view_journal_ListView);
+
+        updateListView();
     }
+    
+    
+    public void updateListView(){
+        
+        String wg_id = Long.toString(activeWG.getActiveWG().getId());
+        
+        String url = "https://wg4u.dnsuser.de/get_wgs_journal.php?wg_id=" + wg_id;
+        
+        VolleyHelper volleyHelper = new VolleyHelper();
+        
+        VolleyHelper.makeStringRequestGET(getApplicationContext(), url, new VolleyResponseListener() {
+            @Override
+            public void onError(String message) {
+                Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onResponse(String response) {
+
+                Log.d(LOG_TAG,response);
+
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    Gson gson = new Gson();
+
+                    journalEntries = new ArrayList<>();
+
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        journalEntries.add(gson.fromJson(jsonArray.getString(i),JournalEntry.class));
+                    }
+
+                    journalAdapter = new JournalAdapter(ShowJournalActivity.this,journalEntries);
+
+                    journalListView.setAdapter(journalAdapter);
+
+                }catch (JSONException e){
+
+                    journalEntries = new ArrayList<>();
+                    journalAdapter = new JournalAdapter(ShowJournalActivity.this,journalEntries);
+
+                    journalListView.setAdapter(journalAdapter);
+                }
+                
+                
+            }
+        });
+
+    }
+    
+    public void onClickAddJournal(View view){
+
+
+        messageInput = findViewById(R.id.input_message);
+
+        if(!isEmpty(messageInput)) {
+
+            String message = messageInput.getText().toString();
+
+            String wg_id = Long.toString(activeWG.getActiveWG().getId());
+
+            String resident_name = activeResident.getActiveResident().getFirstName();
+
+            sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+            SimpleDateFormat stf = new SimpleDateFormat("HH:mm");
+
+            currentDate = sdf.format(new Date());
+
+            String time = stf.format(new Date());
+
+            String[] timeArray = time.split("\\:");
+
+            Map<String, String> params = new HashMap<>();
+
+            params.put("sent_by", resident_name);
+            params.put("wg_id", wg_id);
+            params.put("message", message);
+            params.put("date", currentDate);
+            params.put("hour", timeArray[0]);
+            params.put("minute", timeArray[1]);
+
+            String url = "https://wg4u.dnsuser.de/insert_journal_entry.php";
+
+            VolleyHelper volleyHelper = new VolleyHelper();
+
+            VolleyHelper.makeStringRequestPOST(getApplicationContext(), url, params, new VolleyResponseListener() {
+                @Override
+                public void onError(String message) {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onResponse(String response) {
+                    messageInput.setText("");
+                    updateListView();
+                }
+            });
+        }
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() <= 0;
+    }
+
 }
